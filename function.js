fdd0a27d1d3d42d270cfb7a39046b607d767ecf2
Divyashruti-Mane <69339702+Divyashruti-Mane@users.noreply.github.com>
2020-10-13 19:41:15 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,10 @@
+/* Fuction for zoom in and zoom out*/ 
+var fontSize = 1;
+function zoomIn() {
+	fontSize += 0.1;
+	document.body.style.fontSize = fontSize + "em";
+}
+function zoomOut() {
+	fontSize -= 0.1;
+	document.body.style.fontSize = fontSize + "em";
+}
